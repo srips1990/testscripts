5ff0bea3e9708e18f6d365ed20014c8f3d0232ba
srips1990 <srips1990@gmail.com>
2021-03-30 23:03:59 +0530

    Create xmlhttp.js

---
+++ xmlhttp.js
@@ -0,0 +1,12 @@
+(function () {
+  alert('hi');
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML =
+        this.responseText;
+    }
+  };
+  xhttp.open("GET", "https://w3schools.com/js/ajax_info.txt", true);
+  xhttp.send();
+})()
